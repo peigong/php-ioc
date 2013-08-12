336810ed3186a239125cd4e4d967118e7d2092c7
周培公 <peigong@foxmail.com>
2013-08-12 18:17:55 +0800

    修复了配置文件不存在时报错的BUG。

--- dist/demo/config.inc.php
+++ dist/demo/config.inc.php
@@ -7,4 +7,5 @@ define("ROOT", str_replace("\\", "/", dirname(__FILE__)) . "/");
 require_once(ROOT. "inc/core/ioc/applicationcontextfactory.class.php");
 $applicationContextFactory = ApplicationContextFactory::getIntance();
 $context = $applicationContextFactory->create();
+$context->setConfigPath(ROOT . 'conf/ioc', ROOT);
 ?>
